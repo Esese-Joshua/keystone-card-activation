d8503260de47f7063b7f92e2d50cf68d5e7aa3b4
Esese Joshua <esesejoshua01@gmail.com>
2025-09-11 22:53:28 +0100

    1111!

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import CardActivation from "./components/CardActivation";
 import OtpVerification from "./components/OtpVerification";
 import VerifyAcctNum from "./components/VerifyAcctNum";
 import Home from "./components/Home" 
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
