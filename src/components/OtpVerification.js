802f180bf3db85c90a80659a70910c9209322643
Esese Joshua <esesejoshua01@gmail.com>
2025-09-11 09:34:13 +0100

    worked on the otp input css

--- src/components/OtpVerification.js
+++ src/components/OtpVerification.js
@@ -1,39 +1,68 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import OtpInout from "react-otp-input";
+import OtpInput from "react-otp-input";
+import { ArrowLeft } from "lucide-react"
+import logo from "../img/logo.png"
+import footerCard from "../img/footer-card.jpg"
+import "./OtpVerification.css"
+
 
 const OtpVerification = () => {
-  const [otp, setOtp] = useState("");
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    console.log("OTP entered:", otp);
+  const ProgressIndicator = () => {
+    return(
+      <div className="progress-indicator">
+        <div className="progress-bar active"></div>
+        <div className="progress-bar active"></div>
+        <div className="progress-bar"></div>
+      </div>
+    )
   }
 
-    return (
-        <div className="otp-container">
-            <h2>Secure Card Activation</h2>
-            <p>Enter OTP sent to registered phone number</p>
+  const [otp, setOtp] = useState("");
 
-        <div className="otp-inputs">
-            <h3> One-Time Password {"OTP"} </h3>
-            <OtpInout 
-          value={otp}
-          onChange={setOtp}
-          numInputs={6}
-          renderSeparator={<span>-</span>}
-          renderInput={(props) => <input {...props} />}
-          inputType="tel"
+      return (
+        <div className="container">
+          <div className="header">
+            <ArrowLeft className="arrow-left" />
+              <ProgressIndicator />
+          </div>
+
+          <div className="logo-container">
+            <img src={logo} alt="logo" className="logo"/>
+          </div>
+
+          <div className="text-section">
+            <h2 className="title">Secure Card Activation</h2>
+            <p className="subtitle">Enter OTP sent to registered phone number</p>
+          </div>
+
+        <div className="otp-section">
+            <label className="otp-label"> One-Time Password (OTP) </label>
+            <OtpInput 
+              value={otp}
+              onChange={setOtp}
+              numInputs={6}
+              renderInput={(props) => <input {...props} />}
+              shouldAutoFocus
+              inputType="tel"
+              inputStyle="otp-input"
+              containerStyle="otp-container"
+              // focusStyle=""
             />
         </div>
 
-        <div className="btn-primary">
-            <button> Continue </button>
-
-            <p> Didn't recieve any OTP? <a href="#"> Click to resend </a></p>
+        <div className="timer">
+          <span> 0:20 </span>
         </div>
 
-        <div className="footer"> DEBIT-CARD PIC </div>
+        <button className="btn-primary"> Continue </button>
+
+        <p className="resend-text"> Didn't recieve any OTP? <a href="#"> Click to resend </a></p>
+
+
+        <div className="footer">
+          <img src={footerCard} alt="footer-card" />
+        </div>
     </div>        
     );
 };
